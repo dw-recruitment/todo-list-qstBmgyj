a423cbae68a8bb1842f7f127ef0ca36f7ad96e9d
Anonymous Candidate <anon@example.com>
2016-03-19 15:28:26 -0500

    Completed step 2

--- src/demo_works/core.clj
+++ src/demo_works/core.clj
@@ -1,7 +1,21 @@
 (ns demo-works.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :as params]
+            [ring.util.response :refer :all])
   (:gen-class))
 
+(defn build-form
+  []
+  (str "<!DOCTYPE html><html><body>"
+       "<h1>UNDER CONSTRUCTION!</h1>"
+       "<img src=\"http://media.galaxant.com/000/202/734/pic17564711gif\">"
+       "</body></html>"))
+
+(defn handler
+  "Handle incoming HTTP requests."
+  [request]
+  (response (build-form)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (jetty/run-jetty handler {:port 3000}))
